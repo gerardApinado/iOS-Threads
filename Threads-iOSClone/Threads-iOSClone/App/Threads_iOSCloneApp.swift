ceb600ded48e3ebef58dcd65dec99c3a69a90323
Gerard <gersapinads@gmail.com>
2024-07-10 14:46:35 +0800

    initial commit

---
+++ Threads-iOSClone/Threads-iOSClone/App/Threads_iOSCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_iOSCloneApp.swift
+//  Threads-iOSClone
+//
+//  Created by Gerard on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Threads_iOSCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
